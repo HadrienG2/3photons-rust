bc2d0d04b0db29c98228c52cfbe7c73729050876
Hadrien Grasland <grasland@lal.in2p3.fr>
2020-02-18 14:54:53 +0100

    Fix doc comments

--- src/random/mod.rs
+++ src/random/mod.rs
@@ -1,15 +1,15 @@
 //! Random number generation module. Uses either a port of 3photon's ranf random
 //! number generator or the "rand" crate that is the Rust standard for RNGs.
 
-/// Build the ranf module if it's going to be used
+// Build the ranf module if it's going to be used
 #[cfg(not(feature = "standard-random"))]
 mod ranf;
 
-/// Else mimick the interface of RanfGenerator on top of the rand crate
+// Else mimick the interface of RanfGenerator on top of the rand crate
 #[cfg(feature = "standard-random")]
 mod standard;
 
-// Select the RNG implementation in use
+/// Random Number Generator implementation in use
 #[cfg(not(feature = "standard-random"))]
 pub use self::ranf::RanfGenerator as RandomGenerator;
 #[cfg(feature = "standard-random")]
