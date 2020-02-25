c9c128b967f93ceaef4ee733e24564123f27701e
Hadrien Grasland <grasland@lal.in2p3.fr>
2020-02-25 17:15:24 +0100

    DRY comment

--- src/main.rs
+++ src/main.rs
@@ -134,7 +134,7 @@ fn main() -> Result<()> {
     // Measure how much time has elapsed
     let elapsed_time = start_time.elapsed();
 
-    // Send the results to the standard output and to disk and we're done
+    // Send the results to the standard output and to disk
     output::dump_results(&cfg, &result, elapsed_time).context("Failed to output the results")?;
 
     // ...and we're done
