07353f92c847d04ac7cd0392db4092f1854038de
Hadrien Grasland <grasland@lal.in2p3.fr>
2021-08-24 11:22:46 +0200

    Remove a FIXME which I now disagree with

--- src/event.rs
+++ src/event.rs
@@ -56,9 +56,6 @@ impl Event {
         Self(matrix)
     }
 
-    // FIXME: Remove a big bunch of those and use direct matrix access instead
-    //        once const generics allow us to have elegant matrix slicing.
-
     /// Access the full internal 4-momentum array by reference
     pub fn all_momenta(&self) -> &EventMatrix {
         &self.0
